fa922b1691522442604cd2a00e3eeb3da53f2a87
Nandinisingh07 <nandini.singhaiml2023@indoreinstitute.com>
2026-06-01 01:08:11 +0530

    cleanup: remove debug scripts from frontend

--- frontend/fix_profile.py
+++
@@ -1,7 +0,0 @@
-﻿import sqlite3
-conn = sqlite3.connect('career_ai.db')
-c = conn.cursor()
-c.execute("UPDATE users SET is_profile_complete = 1 WHERE email = 'nandini.singh@indoreinstitute.com'")
-conn.commit()
-conn.close()
-print("Profile marked complete!")
